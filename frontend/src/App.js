92ebfa888f2f0f4801ccff2c3817ebdb201a45e8
Emerson Freitas <emersonfreitasn@gmail.com>
2020-10-30 17:17:56 -0500

    reactjs concepts and adding cors to the backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Header from './components/Header'
 
-import backgroundImg from './assets/background.jpg'
+import api from './services/api'
+
 import './App.css'
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web'])
+  const [projects, setProjects] = useState([])
 
-  function addProject() {
-    // projects.push(`Novo projeto ${Date.now()}`)
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data)
+    })
+  }, [])
 
-    setProjects([...projects, `Novo projeto ${Date.now()}`])
+  async function addProject() {
+    // setProjects([...projects, `Novo projeto ${Date.now()}`])
 
-    console.log(projects)
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Emerson Developer'
+    })
+
+    const project = response.data
+
+    setProjects([...projects, project])
   }
 
   return (
     <>
       <Header title="Homepage" />
 
-      <img src={backgroundImg} alt="Background" width={400} />
-
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title} - {project.owner}</li>)}
       </ul>
 
       <button type="button" onClick={addProject}>Adicionar projeto</button>
